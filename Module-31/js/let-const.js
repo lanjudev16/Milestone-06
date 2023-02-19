c055d05a536848e7ca482a9fd8b0d997056cde1a
lanjudev16 <lanjudev16@gmail.com>
2023-02-19 22:07:57 +0600

    Module-31 2and video

---
+++ Module-31/js/let-const.js
@@ -0,0 +1,20 @@
+//no mor use of var
+// let->it will be change
+// const -> do not allow to reassign
+
+let money=25
+money=35
+console.log(money)
+const bird='jan pakhi'
+// bird='suna pakhi'
+console.log(bird)
+const number=[1,2,3,4,5,6]
+number[1]=9
+number.push(10)
+console.log(number)
+
+const student={
+    name:'mofiz',
+    address:'ongpur'
+}
+student.name="lanju"
