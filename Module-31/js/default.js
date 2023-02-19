39da39140193a7fa491a5b730527bbe12cf01546
lanjudev16 <lanjudev16@gmail.com>
2023-02-19 22:28:47 +0600

    Module-31 3rd video

--- Module-31/js/default.js
+++ Module-31/js/default.js
@@ -1,10 +1,10 @@
 // before es6
-// function add(first,second){
-//     second=second || 0 // if second variable have some value it will assign second and second value is not exits then second value assign 0 
-//     const total=first+second 
-//     return total;
-// }
-// console.log(add(10))
+function add(first,second){
+    second=second || 0 // if second variable have some value it will assign second and second value is not exits then second value assign 0 
+    const total=first+second 
+    return total;
+}
+console.log(add(10))
 
 // after es6
 
