693e2ee06a6e40f6036bd41df9ab27cdc785a3c4
lanjudev16 <lanjudev16@gmail.com>
2023-02-19 23:57:12 +0600

    Module-31 more   arrow function  6th video

---
+++ Module-31/js/moreArrow.js
@@ -0,0 +1,18 @@
+// multi line arrow function .if you return something you must need to return 
+const add=(first,second,third)=>{
+    const sum=first+second
+
+}
+
+// if arrow function no parameter then function like 
+const add1=()=>{
+    // statement
+}
+//if arrow function have one parameter then function like 
+const add2=a=>{
+
+}
+// if arrow function more than one parameter then function like 
+const add3=()=>{
+
+}
