20d5bed69c4390772a0c3bddf215b3d00552a398
lanjudev16 <lanjudev16@gmail.com>
2023-02-20 20:47:48 +0600

    New Module-32 2 no video

--- Module-32/chain.js
+++ Module-32/chain.js
@@ -1,7 +1,40 @@
-const user=[{
-    id:1,
-    name:'Abul',
-    job:'doctor'
-}]
+// const user=[{
+//     id:1,
+//     name:'Abul',
+//     job:'doctor'
+// }]
 
-console.log(user.name)
+// console.log(user)
+
+const data={
+    count:5000,
+    data:[
+        {
+            id:1,
+            name:'babul',
+            job:'leader'
+        },
+        {
+            id:2,
+            name:'dabul',
+            job:'leader'
+        }
+    ]
+}
+const firstJob=data.data[0].job
+// console.log(firstJob)
+
+const user={
+    id:5001,
+    name:'thomas alba edition',
+    address:{
+        street:{
+            firstFlor: 'ka/202 kochuket sagor pari',
+            secondFlor:'green road'
+        },    
+        postOffice:'cantonment',
+        city:'Dhaka'
+    },
+
+}
+console.log(user.address.street.secondFlor)
